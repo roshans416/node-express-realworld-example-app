758361f5fe6a201717886cc5e96f18c8730cd84d
root <root@localhost.localdomain>
2021-02-02 07:19:54 +0800

    Modified app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (!isProduction) {
 if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('mongodb://localhost/conduit');
+  mongoose.connect('mongodb://192.168.94.81/conduit');
   mongoose.set('debug', true);
 }
 
